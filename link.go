3ca26570bf047ab8f21c59ee3c40c8294bfda1d7
baymax <506489196@qq.com>
2023-01-31 17:26:29 +0800

    complete sub 2 conf

---
+++ link.go
@@ -0,0 +1,19 @@
+package sub2config
+
+import (
+	"fmt"
+	"sub2config/utils/ping"
+)
+
+type Link interface {
+	fmt.Stringer
+	Ping(round int, dst string) (ping.Status, error)
+	DestAddr() string
+	Description() string
+	Safe() string
+	Config
+}
+
+type Config interface {
+	Config() map[string]string
+}
